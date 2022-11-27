5a14ded16782d6b8258c2cf2731710e2f31a16c9
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2022-11-27 12:05:58 -0800

    added routes

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,33 @@
 const { Schema, Types } = require('mongoose');
-const reactionSchema = require('./Reaction');
+
+const reactionSchema = new Schema(
+	{
+		reactionId: {
+			type: Schema.Types.ObjectId,
+			default: () => new Types.ObjectId(),
+		},
+		reactionBody: {
+			type: String,
+			required: true,
+			max: 280
+		},
+		userName: {
+			type: String,
+			required: true,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+		},
+	},
+	{
+		toJSON: {
+			virtuals: true,
+		},
+		id: false,
+	}
+);
+
 
 const thoughtSchema = new Schema(
   {
